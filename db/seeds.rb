e61395c54369534f35bfd86db37566541950d9f5
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-04-12 10:45:14 +0500

    add faker gem and generate fake events in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+100.times do
+  Event.create(name: Faker::Name.name)
+end
