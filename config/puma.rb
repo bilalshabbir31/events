a92b766c51bbf4bd038d1f6b0fe2b77a3b52cb40
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-04-21 17:33:04 +0500

    add event in shared file

--- config/puma.rb
+++ config/puma.rb
@@ -56,7 +56,7 @@ unless ENV.fetch('RAILS_ENV', 'development') == 'development'
   #
   preload_app!
 
-  shared_dir = "/home/ubuntu/headhunt/shared"
+  shared_dir = "/home/ubuntu/event/shared"
 
   # Control program(pumactl) socket path
   activate_control_app "unix://#{shared_dir}/#{ENV.fetch("CONTROLFILE", "tmp/sockets/pumactl.sock")}", no_token: true
